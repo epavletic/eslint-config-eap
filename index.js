e57fde0aa46dae04997cc851bb0af1001c8de680
Erik Pavletic <erik.pavletic@gmail.com>
2018-09-24 19:32:12 +0200

    Add extended config for plain js + react.

---
+++ index.js
@@ -0,0 +1,57 @@
+const testFilePattern = '[a-zA-Z]*.test$';
+
+module.exports = {
+  env: {
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  extends: ['airbnb', 'prettier'],
+  parser: 'babel-eslint',
+  plugins: ['filenames', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    curly: ['error', 'all'],
+    'no-useless-constructor': 'error',
+    'no-implicit-coercion': ['error', { boolean: true }],
+  },
+  overrides: [
+    {
+      files: ['**/src/**'],
+      rules: {
+        'import/prefer-default-export': 'off',
+        'no-restricted-syntax': [
+          'error',
+          {
+            selector: 'ForInStatement',
+            message:
+              'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+          },
+          {
+            selector: 'LabeledStatement',
+            message:
+              'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+          },
+          {
+            selector: 'WithStatement',
+            message:
+              '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+          },
+          {
+            files: ['**/src/**/*.js'],
+            excludedFiles: ['*.test.*', 'index.js'],
+            rules: {
+              'filenames/match-regex': 'error',
+            },
+          },
+          {
+            files: ['**/__tests__/**'],
+            rules: {
+              'filenames/match-regex': ['error', testFilePattern],
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
